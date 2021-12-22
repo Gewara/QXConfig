6466fb099101f738c72e6bf968d100d399ef1bf3
Benster <benster@163.com>
2021-12-22 17:33:33 +0800

    美团外卖去开屏广告

--- script/startup.js
+++ script/startup.js
@@ -9,8 +9,8 @@ let magicJS = MagicJS(scriptName, "INFO");
       case /^https?:\/\/wmapi\.meituan\.com\/api\/v\d+\/loadInfo?/.test(magicJS.request.url):
         try {
           let obj = JSON.parse(magicJS.response.body);
-          obj.data.startpicture.ad = [];
-          obj.data.startpicture.mk = [];
+          obj.data.startpicture = [];
+          obj.data.startpicture = [];
           response = { body: JSON.stringify(obj) };
         } catch (err) {
           magicJS.logError(`美团外卖开屏去广告出现异常：${err}`);
