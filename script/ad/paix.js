f032d0eed543e0f0dab80d473b17fe64036d8dcb
Benster <benster@163.com>
2025-11-20 14:08:21 +0800

    update paix.js

--- script/ad/paix.js
+++ script/ad/paix.js
@@ -8,7 +8,7 @@
 /*****************************
 [rewrite_local]
 
-^https?:\/\/stat\.51hchc\.com\/app-config-new\/\d+ url script-analyze-echo-response https://raw.githubusercontent.com/Gewara/QXConfig/main/script/ad/paix.js
+^https?:\/\/stat\.51hchc\.com\/app-config-new\/\d+ url script-response-body https://raw.githubusercontent.com/Gewara/QXConfig/main/script/ad/paix.js
 
 [mitm]
 
@@ -33,6 +33,7 @@ hostname = stat.51hchc.com
       dataObj = {};
     }
 
+    // 首页弹窗广告
     const popups = Array.isArray(dataObj.INDEXTOPPOPUP) ? dataObj.INDEXTOPPOPUP : [];
     popups.forEach(it => {
       if (it && typeof it === 'object') it.enabled = false;
@@ -55,7 +56,6 @@ hostname = stat.51hchc.com
           (item) => item.iconName !== 'public-cup'
         )
       }
-
       if (dataObj.OTHER && typeof dataObj.OTHER === 'object') {
         dataObj.OTHER.indexLanding = { isOpenLoadingImg: false, landingImg: '' }
         // dataObj.OTHER.indexLoading = { isOpenlandingImg: false, loadingImg: '' }
