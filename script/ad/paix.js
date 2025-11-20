7e80f797a475705d32e6b4dca015336584660c10
Benster <benster@163.com>
2025-11-20 11:04:23 +0800

    update.

--- script/ad/paix.js
+++ script/ad/paix.js
@@ -5,13 +5,21 @@
   * update: 2025-11-20
 *******************************/
 
-// request url: https://stat.51hchc.com/app-config-new/15441?timestamp=20251119172546
+/*****************************
+[rewrite_local]
 
-var data = JSON.parse($request.body),
-const INDEXTOPPOPUP = data.INDEXTOPPOPUP || [];
+^https?:\/\/stat\.51hchc\.com\/app-config-new\/\d+ url script-analyze-echo-response https://raw.githubusercontent.com/Gewara/QXConfig/main/script/ad/paix.js
+
+[mitm]
+
+hostname = stat.51hchc.com
+*****************************/
+
+var obj = JSON.parse($request.body),
+const INDEXTOPPOPUP = obj.INDEXTOPPOPUP || [];
 
 INDEXTOPPOPUP.forEach(item => {
   item.enabled = false;
 });
 
-$done({ body: JSON.stringify(data) });
+$done({ body: JSON.stringify(obj) });
