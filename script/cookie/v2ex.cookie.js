053fe709af2f4aa8ae2540ca1a98d579c5b1fa92
Benster <benster@163.com>
2025-11-20 10:51:16 +0800

    document update.

--- script/cookie/v2ex.cookie.js
+++ script/cookie/v2ex.cookie.js
@@ -6,7 +6,7 @@ if (cookieVal) {
   let cookie = $prefs.setValueForKey(cookieVal, cookieKey)
   if (cookie) {
     let msg = `${cookieName}`
-    $notify(msg, 'Cookie写入成功', '详见日志')
+    $notify(msg, 'V2EX Cookie写入成功', '详见日志')
     console.log(msg)
     console.log(cookieVal)
   }
